2bef458bab61e3f8ae6550a04d755c595c161f33
ndc-creator <novocadernovirtual@outlook.com>
2024-10-01 18:37:00 -0300

    Update Country.vue

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,1 +1,15 @@
+<template>
+  <div>
+    <h2>{{ pais.name }}</h2>
+    <p>Capital: {{ pais.capital }}</p>
+    <p>População: {{ pais.population }}</p>
+  </div>
+</template>
 
+<script>
+export default {
+  props: {
+    pais: Object
+  }
+}
+</script>
